fe53aff68b21ead713f302824d15d0894e936222
Arne Kerremans <arnie@pop-os.localdomain>
2024-04-11 09:30:39 +0200

    update mock-login

--- app/routes/mock-login.js
+++ app/routes/mock-login.js
@@ -1,23 +1,26 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class MockLoginRoute extends Route {
+  @service() session;
+  @service() store;
+
   queryParams = {
     page: {
       refreshModel: true,
     },
   };
 
-  @service session;
-  @service store;
-
   beforeModel() {
     this.session.prohibitAuthentication('index');
   }
 
   model(params) {
-    const filter = { provider: 'https://github.com/lblod/mock-login-service' };
-    if (params.gemeente) filter.gebruiker = { achternaam: params.gemeente };
+    const filter = {
+      provider: 'https://github.com/lblod/mock-login-service',
+    };
+    if (params.gemeente)
+      filter.gebruiker = { bestuurseenheden: params.gemeente };
     return this.store.query('account', {
       include: 'gebruiker.bestuurseenheden',
       filter: filter,
