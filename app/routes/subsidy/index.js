3efbcadac53a2ffd2f3526dc2c0011cb49703141
Arne Kerremans <arnie@pop-os.localdomain>
2024-04-16 09:34:12 +0200

    list all subsidy offers

--- app/routes/subsidy/index.js
+++ app/routes/subsidy/index.js
@@ -1,3 +1,37 @@
+/* eslint-disable ember/no-mixins */
 import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { inject as service } from '@ember/service';
 
-export default class SubsidyIndexRoute extends Route {}
+export default class SubsidyIndexRoute extends Route.extend(DataTableRouteMixin) {
+  @service currentSession;
+  @service store;
+
+  modelName = 'subsidy-measure-consumption';
+
+  async model() {
+    let groupId = this.currentSession.group.id;
+    let response = await this.store.query('subsidy-measure-consumption', {
+      include: 'subsidy-measure-offer',
+      filter: {
+        participations: {
+          'participating-bestuurseenheid': {
+            id: groupId,
+          },
+        },
+      },
+    });
+
+    let uniqueSubsidyMeasureOffers = new Map();
+    let uniqueSubsidyMeasureConsumptions = response.slice().filter(consumption => {
+      let offerId = consumption.belongsTo('subsidyMeasureOffer').id();
+      if (!uniqueSubsidyMeasureOffers.has(offerId)) {
+        uniqueSubsidyMeasureOffers.set(offerId, true);
+        return true;
+      }
+      return false;
+    });
+
+    return uniqueSubsidyMeasureConsumptions;
+  }
+}
