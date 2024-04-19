74ae9c80f9ad5a187bfd39793249a51ff0be3e99
Arne Kerremans <140069418+ZijneDwaasheid@users.noreply.github.com>
2024-04-19 10:54:09 +0200

    Update app/routes/subsidy/index.js
    Co-authored-by: wolfderechter <60930264+wolfderechter@users.noreply.github.com>

--- app/routes/subsidy/index.js
+++ app/routes/subsidy/index.js
@@ -6,32 +6,7 @@ import { inject as service } from '@ember/service';
 export default class SubsidyIndexRoute extends Route.extend(DataTableRouteMixin) {
   @service currentSession;
   @service store;
-
-  modelName = 'subsidy-measure-consumption';
-
   async model() {
-    let groupId = this.currentSession.group.id;
-    let response = await this.store.findAll('subsidy-measure-consumption', {
-      include: 'subsidy-measure-offer',
-      filter: {
-        participations: {
-          'participating-bestuurseenheid': {
-            id: groupId,
-          },
-        },
-      },
-    });
-
-    let uniqueSubsidyMeasureOffers = new Map();
-    let uniqueSubsidyMeasureConsumptions = response.slice().filter(consumption => {
-      let offerId = consumption.belongsTo('subsidyMeasureOffer').id();
-      if (!uniqueSubsidyMeasureOffers.has(offerId)) {
-        uniqueSubsidyMeasureOffers.set(offerId, true);
-        return true;
-      }
-      return false;
-    });
-
-    return uniqueSubsidyMeasureConsumptions;
+    let response = await this.store.findAll('subsidy-measure-offer');
+    return response;
   }
-}
